77e9bbe3c481a85b862f99a74be66387c68781d2
Jimmy Tran <quan.tran@metro.digital>
2025-08-13 19:16:32 +0700

    Ensure database connections close on module shutdown

--- packages/database/src/database.service.ts
+++ packages/database/src/database.service.ts
@@ -1,11 +1,11 @@
-import { Injectable, Inject, OnModuleInit } from "@nestjs/common";
+import { Injectable, Inject, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
 import { DB_OPTIONS, DBConfigType } from "./database.interface";
 
 @Injectable()
-export class DatabaseService implements OnModuleInit {
+export class DatabaseService implements OnModuleInit, OnModuleDestroy {
   private pool: Pool;
   public db: ReturnType<typeof drizzle>;
 

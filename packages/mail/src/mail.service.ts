4bdb00aeb0270b1506c9b7d0a36b03ac6f228197
Jimmy Tran <tranlehaiquan@gmail.com>
2025-08-13 20:28:09 +0700

    feat: integrate nodemailer for email handling in MailService; update package dependencies and configuration

--- packages/mail/src/mail.service.ts
+++ packages/mail/src/mail.service.ts
@@ -1,9 +1,29 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
-import {} from "./mail.inferface";
+import { MailConfig } from "./mail.inferface";
+import nodemailer from "nodemailer";
 
 @Injectable()
 export class MailService implements OnModuleInit {
+  private transporter: nodemailer.Transporter;
+
+  constructor(private readonly mailConfig: MailConfig) {
+    this.transporter = nodemailer.createTransport({
+      host: this.mailConfig.host || "smtp.ethereal.email",
+      port: this.mailConfig.port || 587,
+      auth: {
+        user: this.mailConfig.user || "dustin.bechtelar21@ethereal.email",
+        pass: this.mailConfig.pass || "aX8cue9qqNDsBNxyyv",
+      },
+    });
+  }
+
   async onModuleInit() {
-    console.log("MailService initialized");
+    this.transporter.verify((error, success) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Server is ready to take our messages");
+      }
+    });
   }
 }

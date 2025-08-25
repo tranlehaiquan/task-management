049f9dff6eb413bd56aad9089fa1422122bd045b
Q.Tran <quan.tran@metro.digital>
2025-08-25 14:08:08 +0700

    feat: integrate @task-mgmt/eslint-config into multiple services and update pnpm workspace configuration

--- apps/api-gateway/eslint.config.mjs
+++ apps/api-gateway/eslint.config.mjs
@@ -1,35 +1,4 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { createAppEslintConfig } from '@task-mgmt/eslint-config';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      '@typescript-eslint/no-unsafe-return': 'warn'
-    },
-  },
-);
+export default createAppEslintConfig({ tsconfigRootDir: import.meta.dirname });

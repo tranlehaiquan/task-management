7128f1eeb388198ec8275ad2c0c29f5d1c4108ad
Q.Tran <quan.tran@metro.digital>
2025-09-09 13:34:00 +0700

    refactor: remove duplicate imports in main.ts

--- apps/email-worker/src/main.ts
+++ apps/email-worker/src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
-import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
